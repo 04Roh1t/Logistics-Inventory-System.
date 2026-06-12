1dcf1fb1bd9206e7e4dfe7d324f1f41ff3cb1fe5
04Roh1t <rdas2k5@gmail.com>
2026-06-12 20:25:02 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
+
+require('dotenv').config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(cors({ origin: true, credentials: true }));
+app.use(express.json());
+app.use(cookieParser());
+
+app.use('/api/users', require('./routes/authRoutes'));
+app.use('/api/products', require('./routes/productRoutes'));
+app.use('/api/suppliers', require('./routes/supplierRoutes'));
+app.use('/api/history', require('./routes/historyRoutes'));
+app.use('/api/locations', require('./routes/locationRoutes'));
+app.use('/api/stock-movements', require('./routes/stockMovementRoutes'));
+app.use('/api/analytics', require('./routes/analyticsRoutes'));
+app.use('/api/report', require('./routes/reportRoutes'));
+app.use('/api/categories',require('./routes/categoryRoutes'));
+app.use('/api/ai',require('./routes/aiRoutes'));
+
+app.listen(PORT, () => {
+  console.log(`✅ Server running on http://localhost:${PORT}`);
+});
